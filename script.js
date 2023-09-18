adc700fc177e765b5e693e8f23fcb6d964eea67a
Aka yusuf <akayusuf10@gmail.com>
2023-09-18 12:41:06 +0100

    game page

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+ const secretNumber = Math.trunc(Math.random()*20) + 1;
+ document.querySelector('.number').textContent = secretNumber;
+
+ let score = 20;
+
+document.querySelector('.check').addEventListener('click', function(){
+const guess = Number(document.querySelector('.guess',) .value);
+console.log(guess, typeof guess);
+
+if (!guess){
+    document.querySelector('.message').textContent = 'No guess' 
+}else if (guess === secretNumber){
+    document.querySelector('.message').textContent = 'correct number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem'
+}else if (guess > secretNumber){
+    if(score > 1){
+        document.querySelector('.message').textContent = 'number is too high';
+        score--
+        document.querySelector('.score').textContent = score;
+    }else{
+        document.querySelector('.message').textContent = 'You lost';
+        document.querySelector('.score').textContent = 0;
+    }
+}
+else if (guess < secretNumber){
+    if(score > 1){
+        document.querySelector('.message').textContent = 'number is too low';
+        score--
+        document.querySelector('.score').textContent = score;
+    }else{
+        document.querySelector('.message').textContent = 'You lost';
+        document.querySelector('.score').textContent = 0;
+    }
+}
+});  
