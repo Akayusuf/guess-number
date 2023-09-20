fa7d52c914efce93d0ff4e74460cbe57a8627d45
Aka yusuf <akayusuf10@gmail.com>
2023-09-20 12:12:20 +0100

    Game code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
     let secretNumber = Math.trunc(Math.random()*20) + 1;
-    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').textContent = '?';
 
     let score = 20;
     let highscore = 0;
@@ -19,6 +19,7 @@
             displayMessage('correct number');
             document.querySelector('body').style.backgroundColor = '#60b347';
             document.querySelector('.number').style.width = '30rem'
+            document.querySelector('.number').textContent = secretNumber;
 
             if(score > highscore){
                 highscore=score;
